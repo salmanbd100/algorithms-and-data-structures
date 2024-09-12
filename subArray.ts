62ba165fe07da6926a9b9397cd68b25c11915e9e
Salman Rahman <salman.bd100@gmail.com>
2024-09-12 21:24:25 +0600

    sub array

---
+++ subArray.ts
@@ -0,0 +1,17 @@
+function subarraySum(nums: number[], k: number): number {
+  let map = new Map<number, number>([[0, 1]]);
+  let current: number = 0;
+  let matches: number = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    current += nums[i];
+    matches += map.get(current - k) ?? 0;
+
+    map.set(current, (map.get(current) ?? 0) + 1);
+  }
+  console.log(map);
+
+  return matches;
+}
+
+console.log(subarraySum([1, 2, 3], 3));
